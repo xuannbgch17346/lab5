86b936ed3abaacd81d30c30c4a8abe909dc11a69
xuannbgch17346 <xuannbgch17346@fpt.edu.vn>
2022-09-24 04:59:32 -0700

    1ok

---
+++ index.js
@@ -0,0 +1,30 @@
+//khai báo module express framework
+const express = require("express");
+const app = express();
+//khai báo module path
+const path = require("path");
+
+//khai báo server port
+const port = process.env.PORT || 3000;
+
+//set thư mục chứa view
+app.set("view", path.join(__dirname, "views"));
+
+//set view engine (template engine)
+//=> giúp gõ code của back-end ngay trong front-end dễ hơn
+//hbs: handle bar => nếu dùng view engine khác thì thay đổi giá trị này
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/mobile", (req, res)=> {
+  res.render("mobile");
+});
+app.get("/laptop", (req, res)=> {
+  res.render("laptop");
+});
+
+//listen port để chạy server
+app.listen(port);
